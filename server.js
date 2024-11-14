a1451ffec7efc9ef84f38f27f6958ea7b5ca0b17
Melanie Allmond <melanie.allmond@gmail.com>
2024-11-13 22:16:23 -0500

    hooked up band controller

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require("express");
+const { Sequelize } = require("sequelize");
+const app = express();
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Tour API",
+  });
+});
+
+// DATABASE
+const { sequelize } = require("./models");
+// const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`)
+const testSequelize = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+// CONTROLLERS
+const bandsController = require("./controllers/bands_controller");
+app.use("/bands", bandsController);
 
 // LISTEN
-app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, async () => {
+  await testSequelize();
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
